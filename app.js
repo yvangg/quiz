b9ddcb543efd215f03186eddd3383209dfc98939
Iván <yvangg@gmail.com>
2015-07-02 16:45:51 +0200

    Add layout

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -20,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(partials());
 
 app.use('/', routes);
 
