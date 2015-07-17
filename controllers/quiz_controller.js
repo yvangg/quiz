550b4af9243bbc5122826878e5502be92b2cc2bf
Iván García García <yvangg+git@gmail.com>
2015-07-17 19:36:10 +0200

    Multiple questions quiz.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,38 @@
-// GET /quizes/question
-exports.question = function (req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require("../models/models.js");
+
+// Autoload
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if (quiz) {
+				req.quiz = quiz;
+				next();				
+			} else { next(new Error('No existe quizId=' + quizId));}
+		}
+	).catch(function(error) { next(error);});
 };
 
-// GET /quizes/answer
+// GET /quizes/:id
+exports.show = function (req, res) {
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show', {quiz: req.quiz});
+	});	
+};
+
+// GET /quizes/:id/answer
 exports.answer = function (req, res) {
-	if (req.query.respuesta === 'Roma') {
-		res.render('quizes/answer', {respuesta : 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta : 'Incorrecto'});
-	}
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+		if (req.query.respuesta === req.quiz.respuesta) {
+			res.render('quizes/answer', {quiz: req.quiz, respuesta : 'Correcto'});
+		} else {
+			res.render('quizes/answer', {quiz: req.quiz, respuesta : 'Incorrecto'});
+		}
+	});
+};
+
+//GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', {quizes: quizes});
+	}).catch(function(error) { next(error);});
 };
---
+++ models/models.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
+);
+
+
+var sequelize = new Sequelize(null, null, null, 
+					{dialect: "sqlite", storage: "quiz.sqlite"}
+				);
+
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz;
+
+sequelize.sync().success(function() {
+	Quiz.count().success(function(count) {
+		if (count === 0) {
+			Quiz.create({pregunta: 'Capital de Italia',
+						 respuesta: 'Roma'
+						});
+			Quiz.create({pregunta: 'Capital de Portugal',
+						 respuesta: 'Lisboa'
+						})
+			.then(function(){console.log('Base de datos  inicializada')});
+		};
+	});
+});
