58fa70218289559b69de9a82956a838c9d766c10
Iván <yvangg@gmail.com>
2015-07-21 19:39:12 +0200

    Fix models.js

--- models/models.js
+++ models/models.js
@@ -26,11 +26,6 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
-
-var sequelize = new Sequelize(null, null, null, 
-					{dialect: "sqlite", storage: "quiz.sqlite"}
-				);
-
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz;
