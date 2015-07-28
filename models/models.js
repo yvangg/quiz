44b8df20142903270c322477800ffb951bfb5697
Iván <yvangg@gmail.com>
2015-07-28 17:03:28 +0200

    Changes success operations for then

--- models/models.js
+++ models/models.js
@@ -30,8 +30,8 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
 exports.Quiz = Quiz;
 
-sequelize.sync().success(function() {
-	Quiz.count().success(function(count) {
+sequelize.sync().then(function() {
+	Quiz.count().then(function(count) {
 		if (count === 0) {
 			Quiz.create({pregunta: 'Capital de Italia',
 						 respuesta: 'Roma', tema: 'humanidades'
