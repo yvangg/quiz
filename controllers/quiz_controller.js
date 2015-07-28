8c474bb121e31468261a0a6bf87fe09adfb8868b
Iván <yvangg@gmail.com>
2015-07-28 17:10:55 +0200

    Fix quiz_controller

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -31,7 +31,7 @@ exports.answer = function (req, res) {
 };
 
 //GET /quizes
-exports.index = function(req, res) {		
+exports.index = function(req, res, next) {		
 	var search = req.query.search ? '%'+req.query.search+'%' : '%';
 	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
 		res.render('quizes/index.ejs', {quizes: quizes, errors: []});
