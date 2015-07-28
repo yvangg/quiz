42eb1fa2a572b27930f59935608110b2dea3f861
Iván <yvangg@gmail.com>
2015-07-28 16:53:21 +0200

    Add subject of the questions

--- controllers/author_controller.js
+++ controllers/author_controller.js
@@ -1,4 +1,4 @@
 // GET /author
 exports.author = function (req, res) {
-	res.render('author/author');
+	res.render('author/author', {errors: []});
 };
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -39,23 +39,17 @@ exports.index = function(req, res) {
 };
 
 exports.new = function(req, res) {
-	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"});
+	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta", tema: "Otro"});
 	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz);	
-	console.log('test');
-	console.log(quiz.pregunta);
-	console.log(quiz.validate());
-	quiz.validate().then(function() {
-		console.log('validacion');
-	});
 	quiz.validate().then(function(err) {
 		if (err) {
 			res.render('quizes/new', {quiz: quiz, errors: err.errors});
 		} else {
-			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+			quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 				res.redirect('/quizes');
 			});
 		}
@@ -69,11 +63,12 @@ exports.edit = function(req, res) {
 exports.update =  function(req, res) {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 	req.quiz.validate().then(function(err) {
 		if(err) {
 			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 		} else {
-			req.quiz.save({fields: ["pregunta", "respuesta"]})
+			req.quiz.save({fields: ["pregunta", "respuesta", "tema"]})
 			.then(function(){res.redirect('/quizes');});
 		}
 	});
